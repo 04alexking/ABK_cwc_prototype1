cd3897edd105e79f3869cf41cab2f5d24192b29a
04alexking <113943580+04alexking@users.noreply.github.com>
2022-10-26 13:21:24 -0700

    took out a single line of code

--- prototype1/Assets/Scripts/FollowPlayer.cs
+++ prototype1/Assets/Scripts/FollowPlayer.cs
@@ -6,11 +6,6 @@ public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
     public Vector3 offset;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void LateUpdate()
